e30bd30a975c157032ec50410132647dea150ede
Samantaz Fox <coding@samantaz.fr>
2021-03-14 16:16:04 +0100

    Add Xen VM management scripts

---
+++ admin/xen-create-image.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+
+
+DNS_HOSTNAME="example.com"
+
+
+usage()
+{
+	echo "Usage $1 <vCPUs> <RAM> <VM hostname>"
+}
+
+
+cpus=$(echo $1 | sed -E 's/[^0-9]//g')
+memy=$(echo $2 | sed -E 's/[^0-9MG]//g')
+name=$(echo $3 | sed -E 's/[^A-Za-z0-9\-]//g')
+
+if [ -z $cpus ]; then usage "$0"; return 1; fi
+if [ -z $memy ]; then usage "$0"; return 2; fi
+if [ -z $name ]; then usage "$0"; return 3; fi
+
+
+variant=""
+
+do_menu_distro()
+{
+	echo
+	echo "1) Debian"
+	echo "2) Ubuntu"
+	echo
+	echo "q) Exit"
+	echo
+	printf "> "
+
+	read distro
+	case $distro in
+		1) do_menu_debian;;
+		2) do_menu_ubuntu;;
+		q) exit 0;;
+		*) do_menu_distro;;
+	esac
+}
+
+do_menu_debian()
+{
+	echo
+	echo "0) < Back"
+	echo
+	echo "1) Jessie      (8)"
+	echo "2) Bullseye    (9)"
+	echo "3) Buster     (10)"
+	echo "4) Sid  (unstable)"
+	echo
+	echo "q) Exit"
+	echo
+	printf "> "
+
+	read id
+	case $id in
+		0) do_menu_distro;;
+		1) variant="jessie";;
+		2) variant="stretch";;
+		3) variant="buster";;
+		4) variant="sid";;
+		q) exit 0;;
+		*) do_menu_debian;;
+	esac
+}
+
+do_menu_ubuntu()
+{
+	echo
+	echo "0) < Back"
+	echo
+	echo "1) 14.04 - Trusty Tahr"
+	echo "2) 16.04 - Xenial Xerus"
+	echo "3) 17.10 - Artful Aardvark"
+	echo "4) 18.04 - Bionic Beaver"
+	echo "5) 18.10 - Cosmic Cuttlefish"
+	echo "6) 19.04 - Disco Dingo"
+	echo "7) 20.04 - Focal Fossa"
+	echo
+	echo "q) Exit"
+	echo
+	printf "> "
+
+	read id
+	case $id in
+		0) do_menu_distro;;
+		1) variant="trusty";;
+		2) variant="xenial";;
+		3) variant="artful";;
+		4) variant="bionic";;
+		5) variant="cosmic";;
+		6) variant="disco";;
+		7) variant="focal";;
+		q) exit 0;;
+		*) do_distro_ubuntu;;
+	esac
+}
+
+
+do_menu_distro
+if [ -z $variant ]; then echo "No variant selected (error)"; exit 3; fi
+
+xen-create-image \
+	--hostname $name.${DNS_HOSTNAME} \
+	--memory=$memy --vcpus $cpus \
+	--dist $variant --noverbose \
+	--randommac --genpass=1 \
+	--role=builder --role=resolv --role=tmpfs --role=editor
---
+++ admin/xen-delete-image.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+
+DNS_DOMAIN="example.com"
+CFG_DIR="/srv/xen/configs"
+LOG_DIR="/var/log/xen-tools"
+
+
+usage()
+{
+	echo "Usage $1 <VM hostname>"
+}
+
+name=$(basename "$1" .${DNS_DOMAIN})
+name=$(echo $name | sed -E 's/[^A-Za-z0-9\-]//g')
+
+if [ -z $name ]; then usage "$0"; return 1; fi
+
+full_name=${name}.${DNS_DOMAIN}
+
+# Double check if user really wants that
+printf "Do you want to delete '$full_name'? [y/N] "
+read result
+echo
+
+case $result in
+	y|Y) ;; # Continue
+	*)   return 69;;
+esac
+
+# Stop VM
+if ! [ -z $(xl vm-list | grep "$full_name") ]; then
+	xl destroy "$full_name" || return 2
+fi
+
+# Remove disk image
+xen-delete-image "$full_name" || return 3
+
+# Delete logs
+cfg_file=${CFG_DIR}/${full_name}.cfg
+echo "Deleting: $cfg_file"
+test -e "$cfg_file" && rm "$cfg_file"
+
+# Delete config file
+log_file=${LOG_DIR}/${full_name}.log
+echo "Deleting: $log_file"
+test -e "$log_file" && rm "$log_file"
